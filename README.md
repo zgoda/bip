4d3aa40c22d592539b286bb463770ec67723bf4e
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-09-03 14:08:19 +0200

    Initial structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# bip
-Biuletyn Informacji Publicznej
+# Biuletyn Informacji Publicznej
+
+Django application that implements Public Information Bulletin.
+
+Aplikacja Django implementująca Biuletyn Informacji Publicznej (BIP) zgodnie z polskimi wymogami prawnymi.
---
+++ setup.py
@@ -0,0 +1,75 @@
+import codecs
+import re
+from os import path
+
+from setuptools import find_packages, setup
+
+# parts below shamelessly stolen from pypa/pip
+here = path.abspath(path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file,
+        re.M,
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+long_description = read('README.md')
+
+setup(
+    name='biuletyn',
+    version=find_version('src', 'biuletyn', '_version.py'),
+    author='Jarek Zgoda',
+    author_email='jarek.zgoda@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='GPLv3',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    url='http://github.com/zgoda/bip',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: Polish',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Content Management System',
+    ],
+    install_requires=(
+        'Django',
+    ),
+    setup_requires=(
+        'pytest-runner',
+    ),
+    tests_require=(
+        'pytest',
+        'pytest-mock',
+        'pytest-cov',
+        'pytest-factoryboy',
+    ),
+    extras_require={
+        'prod': ['uwsgi']
+    },
+    entry_points={
+        'console_scripts': [
+            'bip=biuletyn.manage:main',
+        ]
+    },
+    python_requires='~=3.7',
+)
