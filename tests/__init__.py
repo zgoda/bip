08134f13a6a5c95b5aa54e3981f7c474edae168f
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-09-29 11:48:04 +0200

    Complete tests for auth views

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,13 @@
 class BIPTests:
 
-    def login(self, name, password):
+    def login(self, name, password, follow_redirects=True):
         data = {
             'username': name,
             'password': password,
         }
-        return self.client.post('/auth/login', data=data, follow_redirects=True)
+        return self.client.post(
+            '/auth/login', data=data, follow_redirects=follow_redirects
+        )
 
-    def logout(self):
-        return self.client.get('/auth/logout', follow_redirects=True)
+    def logout(self, follow_redirects=True):
+        return self.client.get('/auth/logout', follow_redirects=follow_redirects)
--- tests/test_auth_views.py
+++ tests/test_auth_views.py
@@ -15,3 +15,26 @@ class TestAuthViews(BIPTests):
         user = user_factory(name='Ivory Tower', password=password)
         rv = self.login(user.name, password)
         assert 'wyloguj' in rv.text
+
+    def test_login_user_does_not_exist(self):
+        password = 'pass_1'
+        username = 'Ivory Tower'
+        rv = self.login(username, password)
+        assert 'nieprawidłowe dane logowania' in rv.text
+
+    def test_login_wrong_password(self, user_factory):
+        user = user_factory(name='Ivory Tower', password='pass_1')
+        rv = self.login(user.name, 'invalid')
+        assert 'nieprawidłowe dane logowania' in rv.text
+
+    def test_logout_user_logged_in(self, user_factory):
+        password = 'pass_1'
+        user = user_factory(name='Ivory Tower', password=password)
+        self.login(user.name, password)
+        rv = self.logout()
+        assert f'{user.name} wylogowany z systemu' in rv.text
+
+    def test_logout_anonymous(self):
+        rv = self.logout(follow_redirects=False)
+        assert rv.status_code == 302
+        assert url_for('auth.login') in rv.headers['Location']
