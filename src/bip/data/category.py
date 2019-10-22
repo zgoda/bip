09a5867cd04276fe7f3bd50eb37353b7fd1cb593
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-10-22 15:04:19 +0200

    Test for category creation in CLI

--- src/bip/data/category.py
+++ src/bip/data/category.py
@@ -3,11 +3,11 @@ from typing import List, Optional
 from flask_sqlalchemy import BaseQuery
 
 from ..models import Category
-from . import Filter, Sort, create_object, get_query
+from . import Filter, Sort, create_object, get_object, get_query
 
 
 def get(pk: int) -> Optional[Category]:
-    return Category.query.get(pk)
+    return get_object(Category, pk)
 
 
 def create(save: bool = True, **kwargs) -> Category:
--- tests/cli/test_category.py
+++ tests/cli/test_category.py
@@ -1,8 +1,12 @@
+import pytest
+
+from bip.cli.categories.commands import category_create, category_list
+from bip.models import Category
+
 from . import BIPCLITests
 
-from bip.cli.categories.commands import category_list
 
-
+@pytest.mark.usefixtures('app')
 class TestCategoryOps(BIPCLITests):
 
     def test_list_no_categories(self):
@@ -33,3 +37,37 @@ class TestCategoryOps(BIPCLITests):
         assert rv.exit_code == 0
         assert c1.title not in rv.output
         assert c2.title in rv.output
+
+    @pytest.mark.parametrize('active', [True, False], ids=['active', 'inactive'])
+    def test_create_directory(self, active, user_factory, mocker):
+        user = user_factory(name=self.username, password=self.password, admin=True)
+        mocker.patch(
+            'bip.cli.categories.commands.login_user', mocker.Mock(return_value=user)
+        )
+        cat_title = 'test 01'
+        args = ['-t', cat_title, '--directory', '-u', user.name]
+        if active:
+            args.append('--active')
+        rv = self.runner.invoke(category_create, args)
+        assert rv.exit_code == 0
+        assert 'została utworzona' in rv.output
+        cat_obj = Category.query.filter_by(title=cat_title).one()
+        assert cat_obj.directory is not None
+        assert cat_obj.active is active
+
+    @pytest.mark.parametrize('active', [True, False], ids=['active', 'inactive'])
+    def test_create_no_directory(self, active, user_factory, mocker):
+        user = user_factory(name=self.username, password=self.password, admin=True)
+        mocker.patch(
+            'bip.cli.categories.commands.login_user', mocker.Mock(return_value=user)
+        )
+        cat_title = 'test 01'
+        args = ['-t', cat_title, '-u', user.name]
+        if active:
+            args.append('--active')
+        rv = self.runner.invoke(category_create, args)
+        assert rv.exit_code == 0
+        assert 'została utworzona' in rv.output
+        cat_obj = Category.query.filter_by(title=cat_title).one()
+        assert cat_obj.directory is None
+        assert cat_obj.active is active
