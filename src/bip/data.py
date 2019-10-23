09009e64320c889345568f3541a6a1e4bb411fdf
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-10-23 21:21:44 +0200

    Final take on code duplication in DAL

---
+++ src/bip/data.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+from collections import namedtuple
+from typing import Any, List, Optional, Type, Union
+
+from flask_sqlalchemy import BaseQuery, Model
+from sqlalchemy_filters import apply_filters, apply_sort
+
+from .ext import db
+from .models import Category, Directory, Page, User, ChangeRecord, ChangeType
+from .utils.http import or_404
+
+Filter = namedtuple('Filter', 'field,op,value,model', defaults=(None,))
+
+Sort = namedtuple(
+    'Sort', 'field,direction,model,nullsfirst,nullslast',
+    defaults=('asc', None, None, None),
+)
+
+
+class AccessObject:
+
+    klass_ = None
+
+    @classmethod
+    def for_class(cls, klass_: Type[Model]) -> AccessObject:
+        obj = cls()
+        obj.klass_ = klass_
+        return obj
+
+    def create(self, save: bool = True, **kwargs) -> Model:
+        obj = self.klass_(**kwargs)
+        if save:
+            db.session.add(obj)
+            db.session.commit()
+        return obj
+
+    def get(self, pk: Any, abort_on_none: bool = False) -> Model:
+        obj = self.klass_.query.get(pk)
+        if abort_on_none:
+            return or_404(obj)
+        return obj
+
+    def query(
+                self, sort: Optional[List[Sort]] = None,
+                filters: Optional[List[Filter]] = None,
+            ) -> BaseQuery:
+        q = self.klass_.query
+        if filters:
+            q = apply_filters(q, [f._asdict() for f in filters])
+        if sort:
+            q = apply_sort(q, [s._asdict() for s in sort])
+        return q
+
+
+class PageAccessObject(AccessObject):
+
+    @classmethod
+    def make(cls) -> PageAccessObject:
+        return cls.for_class(Page)
+
+    def category_names(self, page: Page) -> List[str]:
+        return [c.title for c in page.categories]
+
+    def directory_names(self, page: Page) -> List[str]:
+        return [d.title for d in page.directories]
+
+
+class UserAccessObject(AccessObject):
+
+    @classmethod
+    def make(cls) -> UserAccessObject:
+        return cls.for_class(User)
+
+    def create(self, save: bool = True, **kwargs) -> User:
+        password = kwargs.pop('password')
+        user = super().create(save=False, **kwargs)
+        user.set_password(password)
+        if save:
+            db.session.add(user)
+            db.session.commit()
+        return user
+
+    def by_name(self, name: str, **params) -> Optional[User]:
+        return self.klass_.query.filter_by(name=name, **params).first()
+
+    def get_or_404(self, pk: int) -> User:
+        return self.get(pk, abort_on_none=True)
+
+
+class ChangeAccessObject(AccessObject):
+
+    @classmethod
+    def make(cls) -> ChangeAccessObject:
+        return cls.for_class(ChangeRecord)
+
+    def record(
+                self, obj: Any, change_type: Union[ChangeType, str], user: User,
+                description: str,
+            ) -> ChangeRecord:
+        if isinstance(change_type, str):
+            change_type = ChangeType[change_type]
+        return self.klass_.log_change(obj, change_type, user, description)
+
+
+directory = AccessObject.for_class(Directory)
+category = AccessObject.for_class(Category)
+page = PageAccessObject.make()
+user = UserAccessObject.make()
+change = ChangeAccessObject.make()
