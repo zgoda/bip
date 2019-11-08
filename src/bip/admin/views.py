e46604e5043e350112581c1334b87a3966dbd3ec
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-11-08 09:11:00 +0100

    Inline some objects

--- src/bip/admin/views.py
+++ src/bip/admin/views.py
@@ -25,55 +25,64 @@ def home() -> Response:
 
 @admin_bp.route('/users/list')
 def user_list() -> Response:
-    user_list_meta = ItemCollectionMeta(dataobject=user, order=[Sort(field='name')])
-    return default_admin_list_view(user_list_meta)
+    return default_admin_list_view(
+        ItemCollectionMeta(dataobject=user, order=[Sort(field='name')])
+    )
 
 
 @admin_bp.route('/users/<int:user_pk>', methods=['POST', 'GET'])
-def user_detail(user_pk) -> Response:
-    user_item_meta = ItemMeta(
-        dataobject=user, form=UserForm,
-        message='dane użytkownika {obj_name} zostały zmienione', title_field='name',
-        success_url='admin.user_list'
+def user_detail(user_pk: int) -> Response:
+    return default_admin_item_view(
+        ItemMeta(
+            dataobject=user, form=UserForm,
+            message='dane użytkownika {obj_name} zostały zmienione',
+            title_field='name', success_url='admin.user_list',
+        ),
+        user_pk,
     )
-    return default_admin_item_view(user_item_meta, user_pk)
 
 
 @admin_bp.route('/category/list')
 def category_list() -> Response:
-    category_list_meta = ItemCollectionMeta(
-        dataobject=category,  order=[Sort(field='menu_order'), Sort(field='title')],
+    return default_admin_list_view(
+        ItemCollectionMeta(
+            dataobject=category,  order=[Sort(field='menu_order'), Sort(field='title')]
+        )
     )
-    return default_admin_list_view(category_list_meta)
 
 
 @admin_bp.route('/category/<int:category_pk>', methods=['POST', 'GET'])
-def category_detail(category_pk) -> Response:
+def category_detail(category_pk: int) -> Response:
     form_queries = {
         'parent': category.query(
             sort=[Sort(field='title')],
             filters=[Filter(field='pk', op='ne', value=category_pk)],
         )
     }
-    category_item_meta = ItemMeta(
-        dataobject=category, form=CategoryForm,
-        message='dane kategorii {obj_name} zostały zmienione', title_field='title',
-        success_url='admin.category_list', form_queries=form_queries,
+    return default_admin_item_view(
+        ItemMeta(
+            dataobject=category, form=CategoryForm,
+            message='dane kategorii {obj_name} zostały zmienione', title_field='title',
+            success_url='admin.category_list', form_queries=form_queries,
+        ),
+        category_pk,
     )
-    return default_admin_item_view(category_item_meta, category_pk)
 
 
 @admin_bp.route('/page/list')
 def page_list() -> Response:
-    page_list_meta = ItemCollectionMeta(dataobject=page, order=[Sort(field='title')])
-    return default_admin_list_view(page_list_meta)
+    return default_admin_list_view(
+        ItemCollectionMeta(dataobject=page, order=[Sort(field='title')])
+    )
 
 
 @admin_bp.route('/page/<int:page_pk>', methods=['POST', 'GET'])
-def page_detail(page_pk) -> Response:
-    page_item_meta = ItemMeta(
-        dataobject=page, form=PageForm,
-        message='dane strony {obj_name} zostały zmienione', title_field='title',
-        success_url='admin.page_list',
+def page_detail(page_pk: int) -> Response:
+    return default_admin_item_view(
+        ItemMeta(
+            dataobject=page, form=PageForm,
+            message='dane strony {obj_name} zostały zmienione',
+            title_field='title', success_url='admin.page_list',
+        ),
+        page_pk,
     )
-    return default_admin_item_view(page_item_meta, page_pk)
