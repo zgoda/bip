05661e0e4941be8f06aa0c924700763194af97a6
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-01-29 20:38:03 +0100

    Page view

--- src/bip/main/views.py
+++ src/bip/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 
 from . import main_bp
 
+from ..data import page
+
 
 @main_bp.route('/')
 def home():
@@ -23,6 +25,7 @@ def contact():
     return render_template('main/contact.html')
 
 
-@main_bp.route('/<int:page_id>')
-def page(page_id: int):
-    return render_template('main/page.html')
+@main_bp.route('/<int:page_id>', endpoint='page')
+def page_view(page_id: int):
+    page_obj = page.get(page_id, abort_on_none=True)
+    return render_template('main/page.html', page=page_obj)
