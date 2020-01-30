da09fedc56a00f6c2e4c25667e43b550e5b75b44
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-01-30 15:46:20 +0100

    Move factories to single module

--- tests/factories.py
+++ tests/factories.py
@@ -2,9 +2,21 @@ import factory
 from factory.alchemy import SQLAlchemyModelFactory
 
 from bip.ext import db
-from bip.models import Page
+from bip.models import User, Page
 
-from .user import UserFactory
+
+class UserFactory(SQLAlchemyModelFactory):
+
+    name = factory.Faker('name')
+    email = factory.Faker('email')
+    password = factory.PostGenerationMethodCall('set_password')
+    active = True
+    admin = False
+
+    class Meta:
+        model = User
+        sqlalchemy_session = db.session
+        sqlalchemy_session_persistence = 'flush'
 
 
 class PageFactory(SQLAlchemyModelFactory):
--- tests/factories/__init__.py
+++
@@ -1,2 +0,0 @@
-from .user import UserFactory  # noqa: F401
-from .page import PageFactory  # noqa: F401
