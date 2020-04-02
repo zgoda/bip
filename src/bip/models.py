7a51c0ed8df5f3b9985228a5d4ae7818969c597c
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-04-02 19:28:24 +0200

    Back to normal (not pooled) connection.
    Pooled connection appears rather unstable if any exception is raised -
    the connection seems to not be returned to the pool if Werkzeug debugger
    handles exception and reloader is active.

--- src/bip/models.py
+++ src/bip/models.py
@@ -9,21 +9,18 @@ from peewee import (
     AutoField, BooleanField, CharField, DateTimeField, ForeignKeyField, IntegerField,
     TextField,
 )
-from playhouse.pool import (
-    PooledMySQLDatabase, PooledPostgresqlDatabase, PooledSqliteDatabase,
-)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 DB_DRIVER_MAP = {
-    'postgres': PooledPostgresqlDatabase,
-    'mysql': PooledMySQLDatabase,
+    'postgres': peewee.PostgresqlDatabase,
+    'mysql': peewee.MySQLDatabase,
 }
 
 
 def _get_db_driver_class():
     name = os.getenv('DB_DRIVER')
     if name is None:
-        return PooledSqliteDatabase
+        return peewee.SqliteDatabase
     return DB_DRIVER_MAP[name]
 
 
