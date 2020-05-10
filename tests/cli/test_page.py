89dc58c269797d0a0134baf535120bc00e1831a8
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-05-10 23:04:15 +0200

    Small speedup of test runs

--- tests/cli/test_page.py
+++ tests/cli/test_page.py
@@ -1,7 +1,7 @@
 import pytest
 
 from bip.cli.pages.commands import page_change, page_create, page_labels, page_list
-from bip.models import Page
+from bip.models import Page, PageLabel
 from bip.utils.text import slugify, truncate_string
 
 from . import BIPCLITests
@@ -10,50 +10,53 @@ from . import BIPCLITests
 @pytest.mark.usefixtures('app')
 class TestPageOps(BIPCLITests):
 
+    @pytest.fixture(autouse=True)
+    def set_up2(self, user_factory):
+        self.user = user_factory(name=self.username, password=self.password)
+
     def test_list_no_pages(self):
         rv = self.runner.invoke(page_list)
         assert rv.exit_code == 0
         assert 'żadnych stron' in rv.output
 
     def test_list_all(self, page_factory):
-        p1 = page_factory(active=True)
-        p2 = page_factory(active=False)
+        p1 = page_factory(active=True, created_by=self.user, updated_by=self.user)
+        p2 = page_factory(active=False, created_by=self.user, updated_by=self.user)
         rv = self.runner.invoke(page_list)
         assert rv.exit_code == 0
         assert truncate_string(p1.title, 80) in rv.output
         assert truncate_string(p2.title, 80) in rv.output
 
     def test_list_active_only(self, page_factory):
-        p1 = page_factory(active=True)
-        p2 = page_factory(active=False)
+        p1 = page_factory(active=True, created_by=self.user, updated_by=self.user)
+        p2 = page_factory(active=False, created_by=self.user, updated_by=self.user)
         rv = self.runner.invoke(page_list, ['--active'])
         assert rv.exit_code == 0
         assert truncate_string(p1.title, 80) in rv.output
         assert truncate_string(p2.title, 80) not in rv.output
 
     def test_list_inactive_only(self, page_factory):
-        p1 = page_factory(active=True)
-        p2 = page_factory(active=False)
+        p1 = page_factory(active=True, created_by=self.user, updated_by=self.user)
+        p2 = page_factory(active=False, created_by=self.user, updated_by=self.user)
         rv = self.runner.invoke(page_list, ['--inactive'])
         assert rv.exit_code == 0
         assert truncate_string(p1.title, 80) not in rv.output
         assert truncate_string(p2.title, 80) in rv.output
 
-    def test_create_ok_simple(self, mocker, user_factory):
+    def test_create_ok_simple(self, mocker):
         title = 'Tytuł strony 1'
-        actor = user_factory(name=self.username, password=self.password)
-        fake_login = mocker.Mock(return_value=actor)
+        fake_login = mocker.Mock(return_value=self.user)
         mocker.patch('bip.cli.pages.commands.login_user', fake_login)
         fake_edit = mocker.Mock(return_value=title)
         mocker.patch('bip.cli.pages.commands.click.edit', fake_edit)
-        rv = self.runner.invoke(page_create, ['-t', title, '-u', actor.name])
+        rv = self.runner.invoke(page_create, ['-t', title, '-u', self.user.name])
         assert f'strona {title} została utworzona' in rv.output
 
-    def test_create_ok_no_description(self, mocker, user_factory):
+    def test_create_ok_no_description(self, mocker):
         title = 'Tytuł strony 1'
-        actor = user_factory(name=self.username, password=self.password)
         mocker.patch(
-            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
+            'bip.cli.pages.commands.login_user',
+            mocker.Mock(return_value=self.user),
         )
         mocker.patch(
             'bip.cli.pages.commands.click.edit', mocker.Mock(return_value=title)
@@ -61,18 +64,17 @@ class TestPageOps(BIPCLITests):
         mocker.patch(
             'bip.cli.pages.commands.click.confirm', mocker.Mock(return_value=False)
         )
-        rv = self.runner.invoke(page_create, ['-t', title, '-u', actor.name])
+        rv = self.runner.invoke(page_create, ['-t', title, '-u', self.user.name])
         assert f'strona {title} została utworzona' in rv.output
         page = Page.get(Page.title == title)
         assert page.description is None
 
-    def test_create_ok_with_description(self, mocker, user_factory):
+    def test_create_ok_with_description(self, mocker):
         title = 'Tytuł strony 1'
         text = 'Treść strony 1'
         description = 'Opis strony 1'
-        actor = user_factory(name=self.username, password=self.password)
         mocker.patch(
-            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
+            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=self.user)
         )
         mocker.patch(
             'bip.cli.pages.commands.click.edit',
@@ -81,13 +83,13 @@ class TestPageOps(BIPCLITests):
         mocker.patch(
             'bip.cli.pages.commands.click.confirm', mocker.Mock(return_value=True)
         )
-        rv = self.runner.invoke(page_create, ['-t', title, '-u', actor.name])
+        rv = self.runner.invoke(page_create, ['-t', title, '-u', self.user.name])
         assert f'strona {title} została utworzona' in rv.output
         page = Page.get(Page.title == title)
         assert page.text == text
         assert page.description == description
 
-    def test_create_ok_with_labels(self, mocker, user_factory, label_factory):
+    def test_create_ok_with_labels(self, mocker, label_factory):
         label_a = 'Etykieta A'
         label_factory(name=label_a)
         label_b = 'Etykieta B'
@@ -95,9 +97,8 @@ class TestPageOps(BIPCLITests):
         title = 'Tytuł strony 1'
         text = 'Treść strony 1'
         description = 'Opis strony 1'
-        actor = user_factory(name=self.username, password=self.password)
         mocker.patch(
-            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
+            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=self.user)
         )
         mocker.patch(
             'bip.cli.pages.commands.click.edit',
@@ -107,7 +108,8 @@ class TestPageOps(BIPCLITests):
             'bip.cli.pages.commands.click.confirm', mocker.Mock(return_value=True)
         )
         rv = self.runner.invoke(
-            page_create, ['-t', title, '-u', actor.name, '-l', label_a, '-l', label_b]
+            page_create,
+            ['-t', title, '-u', self.user.name, '-l', label_a, '-l', label_b],
         )
         assert f'strona {title} została utworzona' in rv.output
         page = Page.get(Page.title == title)
@@ -115,61 +117,57 @@ class TestPageOps(BIPCLITests):
         pagelabels = [x.label.name for x in page.labels()]
         assert all([label_a in pagelabels, label_b in pagelabels])
 
-    def test_create_fail_no_text(self, mocker, user_factory):
+    def test_create_fail_no_text(self, mocker):
         title = 'Tytuł strony 1'
-        actor = user_factory(name=self.username, password=self.password)
         mocker.patch(
-            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
+            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=self.user)
         )
         mocker.patch('bip.cli.pages.commands.click.edit', mocker.Mock(return_value=''))
-        rv = self.runner.invoke(page_create, ['-t', title, '-u', actor.name])
+        rv = self.runner.invoke(page_create, ['-t', title, '-u', self.user.name])
         assert rv.exit_code != 0
         assert 'Tekst strony jest wymagany' in rv.output
 
-    def test_change_noop(self, mocker, user_factory, page_factory):
-        actor = user_factory(name=self.username, password=self.password)
+    def test_change_noop(self, mocker, page_factory):
         mocker.patch(
-            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
+            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=self.user)
         )
-        page = page_factory()
-        rv = self.runner.invoke(page_change, ['-i', page.pk, '-u', actor.name])
+        page = page_factory(created_by=self.user, updated_by=self.user)
+        rv = self.runner.invoke(page_change, ['-i', page.pk, '-u', self.user.name])
         assert 'nie wprowadzono żadnych zmian' in rv.output
 
-    def test_change_title(self, mocker, user_factory, page_factory):
-        actor = user_factory(name=self.username, password=self.password)
+    def test_change_title(self, mocker, page_factory):
         mocker.patch(
-            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
+            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=self.user)
         )
         title = 'Tytuł strony 1'
-        page = page_factory(title=title)
+        page = page_factory(title=title, created_by=self.user, updated_by=self.user)
         new_title = 'Nowy tytuł'
         rv = self.runner.invoke(
-            page_change, ['-i', page.pk, '-u', actor.name, '-t', new_title]
+            page_change, ['-i', page.pk, '-u', self.user.name, '-t', new_title]
         )
         assert 'została zmieniona' in rv.output
         page_obj = Page[page.pk]
         assert page_obj.title == new_title
         assert page_obj.slug == slugify(new_title)
 
-    def test_change_active(self, mocker, user_factory, page_factory):
-        actor = user_factory(name=self.username, password=self.password)
+    def test_change_active(self, mocker, page_factory):
         mocker.patch(
-            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
+            'bip.cli.pages.commands.login_user', mocker.Mock(return_value=self.user)
         )
-        page = page_factory(active=False)
+        page = page_factory(active=False, created_by=self.user, updated_by=self.user)
         rv = self.runner.invoke(
-            page_change, ['-i', page.pk, '-u', actor.name, '--active']
+            page_change, ['-i', page.pk, '-u', self.user.name, '--active']
         )
         assert 'została zmieniona' in rv.output
         page_obj = Page[page.pk]
         assert page_obj.active is True
 
-    def test_change_main(self, mocker, user_factory, page_factory):
-        actor = user_factory(name=self.username, password=self.password)
+    def test_change_main(self, mocker, page_factory):
+        actor = self.user
         mocker.patch(
             'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
         )
-        page = page_factory(main=False)
+        page = page_factory(main=False, created_by=actor, updated_by=actor)
         rv = self.runner.invoke(
             page_change, ['-i', page.pk, '-u', actor.name, '--main']
         )
@@ -177,12 +175,12 @@ class TestPageOps(BIPCLITests):
         page_obj = Page[page.pk]
         assert page_obj.main is True
 
-    def test_change_order(self, mocker, user_factory, page_factory):
-        actor = user_factory(name=self.username, password=self.password)
+    def test_change_order(self, mocker, page_factory):
+        actor = self.user
         mocker.patch(
             'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
         )
-        page = page_factory()
+        page = page_factory(created_by=actor, updated_by=actor)
         new_order = 2
         rv = self.runner.invoke(
             page_change, ['-i', page.pk, '-u', actor.name, '-o', str(new_order)]
@@ -191,24 +189,25 @@ class TestPageOps(BIPCLITests):
         page_obj = Page[page.pk]
         assert page_obj.order == new_order
 
-    def test_labels_add_none(self, mocker, user_factory, page_factory):
-        actor = user_factory(name=self.username, password=self.password)
+    def test_labels_add_none(self, mocker, page_factory):
+        actor = self.user
         mocker.patch(
             'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
         )
-        page = page_factory()
+        page = page_factory(created_by=actor, updated_by=actor)
         rv = self.runner.invoke(
             page_labels, ['-i', page.pk, '-o', 'add', '-u', actor.name]
         )
         assert f'etykiety strony {page.title} nie zostały zmienione' in rv.output
         assert rv.exit_code == 0
+        assert PageLabel.select().filter(PageLabel.page == page).count() == 0
 
-    def test_labels_add_one(self, mocker, user_factory, page_factory, label_factory):
-        actor = user_factory(name=self.username, password=self.password)
+    def test_labels_add_one(self, mocker, page_factory, label_factory):
+        actor = self.user
         mocker.patch(
             'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
         )
-        page = page_factory()
+        page = page_factory(created_by=actor, updated_by=actor)
         label = label_factory(name='etykieta 1')
         rv = self.runner.invoke(
             page_labels,
@@ -216,14 +215,15 @@ class TestPageOps(BIPCLITests):
         )
         assert rv.exit_code == 0
         assert f'etykiety strony {page.title} zostały zaktualizowane' in rv.output
+        assert PageLabel.select().filter(PageLabel.page == page).count() == 1
 
-    def test_labels_add_many(self, mocker, user_factory, page_factory, label_factory):
+    def test_labels_add_many(self, mocker, page_factory, label_factory):
         num_labels = 8
-        actor = user_factory(name=self.username, password=self.password)
+        actor = self.user
         mocker.patch(
             'bip.cli.pages.commands.login_user', mocker.Mock(return_value=actor)
         )
-        page = page_factory()
+        page = page_factory(created_by=actor, updated_by=actor)
         labels = label_factory.create_batch(num_labels)
         call_args = ['-i', page.pk, '-o', 'add', '-u', actor.name]
         for label in labels:
@@ -231,3 +231,4 @@ class TestPageOps(BIPCLITests):
         rv = self.runner.invoke(page_labels, call_args)
         assert rv.exit_code == 0
         assert f'etykiety strony {page.title} zostały zaktualizowane' in rv.output
+        assert PageLabel.select().filter(PageLabel.page == page).count() == num_labels
