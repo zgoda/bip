38e807540e9e41ab477711242a01fbf4ddd50038
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-07-22 21:06:00 +0200

    Generate save file name for attachments

--- src/bip/models.py
+++ src/bip/models.py
@@ -11,6 +11,8 @@ from peewee import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from .utils.text import slugify
+
 DB_DRIVER_MAP = {
     'postgres': peewee.PostgresqlDatabase,
     'mysql': peewee.MySQLDatabase,
@@ -139,3 +141,8 @@ class Attachment(Model):
     title = CharField(max_length=200, index=True)
     description = TextField(null=True)
     description_html = TextField(null=True)
+
+    @property
+    def file_save_as(self):
+        _, ext = os.path.splitext(self.filename)
+        return f'{slugify(self.title)}{ext}'
