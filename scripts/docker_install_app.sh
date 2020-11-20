0c5c11819c57e867556fcc51ef58a2af7f834d00
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-11-20 23:46:39 +0100

    Do not install in editable mode, we don't have any source

--- scripts/docker_install_app.sh
+++ scripts/docker_install_app.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-pip install --user -U --no-cache-dir --no-warn-script-location -e .
+pip install --user -U --no-cache-dir --no-warn-script-location .
 
 pip install --user -U --no-cache-dir --no-warn-script-location gunicorn
