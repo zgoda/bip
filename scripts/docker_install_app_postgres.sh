44b05412c8bed34527da56629ff7f1be404571c8
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-11-25 15:57:03 +0100

    Move Dockerfiles to separate directory

---
+++ scripts/docker_install_app_postgres.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -euo pipefail
+
+pip install --user -U --no-cache-dir --no-warn-script-location pip wheel setuptools Cython
+
+pip install --user -U --no-cache-dir --no-warn-script-location .
+
+pip install --user -U --no-cache-dir --no-warn-script-location gunicorn psycopg2-binary
