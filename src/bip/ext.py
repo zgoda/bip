56e3a305b28e3df478571fd5feb422daf6869b6e
Jarek Zgoda <jarek.zgoda@gmail.com>
2022-02-07 12:43:29 +0100

    Bootstrap-Flask 2.x compat

--- src/bip/ext.py
+++ src/bip/ext.py
@@ -1,9 +1,9 @@
 from flask_babel import Babel
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap4
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 
 babel = Babel(default_locale='pl', default_timezone='Europe/Warsaw')
 login_manager = LoginManager()
 csrf = CSRFProtect()
-bootstrap = Bootstrap()
+bootstrap = Bootstrap4()
